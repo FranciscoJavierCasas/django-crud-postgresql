bb8100a42f9af4ab491ee112a8f3455cc83d896a
FranciscoJavierCasas <francisco.casas@correounivalle.edu.co>
2023-02-14 09:47:02 -0500

    Proyecto de crud con postgres

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import list_tasks, create_tasks,delete_task
+
+urlpatterns = [
+    path('',list_tasks),
+    path('new/',create_tasks, name="create_tasks"),
+    path('delete/<int:task_id>/',delete_task, name="delete_task"),
+]
